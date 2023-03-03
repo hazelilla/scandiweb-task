46678ffa2f5debee8649359a954bc20394df9d7e
hazelilla <elanur-1998@live.com>
2023-03-04 00:54:57 +0100

    Switch options added

--- src/pages/product/ProductPage.js
+++ src/pages/product/ProductPage.js
@@ -12,6 +12,57 @@ function ProductPage() {
     navigate('/');
   };
 
+  const [selectedType, setSelectedType] = useState("");
+
+  const handleTypeChange = (event) => {
+    setSelectedType(event.target.value);
+  };
+
+  const renderInput = () => {
+    if (selectedType === "option1") {
+      return (
+        <div>
+            <div className="inline">
+                <p className="tags">Size (MB)</p>
+                <input className="input-style" />
+            </div>
+            <p className="description">Please, provide disc space in MB</p>
+        </div>
+        
+      );
+    } else if (selectedType === "option2"){
+      return (
+        <div>
+            <div className="inline">
+                <p className="tags">Weight (KG)</p>
+                <input className="input-style"></input>
+            </div>
+            <p className="description">Please, provide weight in KG</p>
+        </div>
+        
+      );
+    } else if (selectedType === "option3"){
+        return (
+            <div>
+                <div className="inline">
+                    <p className="tags">Height (CM)</p>
+                    <input className="input-style"></input>
+                </div>
+                <div className="inline">
+                    <p className="tags">Width (CM)</p>
+                    <input className="input-style"></input>
+                </div>
+                <div className="inline">
+                    <p className="tags">Length (CM)</p>
+                    <input className="input-style"></input>
+                </div>
+
+                <p className="description">Please, provide dimensions in HxWxL format</p>
+            </div>
+        );
+    }
+  };
+
   return (
     <div className="home">
 
@@ -49,16 +100,15 @@ function ProductPage() {
 
         <div className="inline">
             <p>Type Switcher</p>
-            <select className="select">
+            <select className="select" onChange={handleTypeChange}>
                 <option value="">Type Switcher</option>
                 <option value="option1">DVD-disc</option>
                 <option value="option2">Book</option>
                 <option value="option3">Furniture</option>
             </select>
         </div>
-        
-        
-   
+        {renderInput()}
+
       </div>
 
       {/* Footer */}
